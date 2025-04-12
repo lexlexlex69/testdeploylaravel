bf77136ebaf1c617f66a2cabcf47cc4cbde12bdc
tescoph2025 <tescoph2025@gmail.com>
2025-04-12 19:19:39 +0800

    dfdfdfd

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+//fdsfsdf
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,16 +15,15 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
 Route::get('/userpublic', [UserController::class, 'index2']);
-Route::middleware('auth:sanctum')->group(function(){
-    Route::get('/user', function (Request $request){
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
         return $request->user();
     });
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('/users', UserController::class);
-
 });
 
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/login', [AuthController::class, 'login']);
-
